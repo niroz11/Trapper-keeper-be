4fbd2090aff2507152a03a1a2592a49e3e1bc022
Jarrett Kong <jarrettkong@me.com>
2019-05-21 16:21:48 -0600

    Combine app and server into one file

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-import app from './app'
+import express from 'express';
+import cors from 'cors';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.set('port', 3001);
 
-app.listen(app.get('port'), () => {
-  console.log(`App is running on http://localhost:${app.get('port')}`)
-})
+app.locals.title = 'Trapper Keeper';
+app.locals.notes = [];
+app.locals.items = [];
+
+app.get('/api/v1/notes', (req, res) => {
+	const notes = app.locals.notes;
+	const items = app.locals.items;
+	return res.status(200).json({ notes, items });
+});
+
+app.listen(app.get('port'), () => console.log(`Listening on port ${app.get('port')}`));
