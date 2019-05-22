09a4fa5862b6cfd1458f0990680fe66cafa30baa
Jarrett Kong <jarrettkong@me.com>
2019-05-21 20:42:51 -0600

    Add paths for restful routing

--- server.js
+++ server.js
@@ -7,11 +7,56 @@ app.use(express.json());
 
 app.locals.title = 'Trapper Keeper';
 app.locals.notes = [];
-app.locals.items = [];
+app.locals.lists = [];
+
+app.get('/api/v1/lists', (req, res) => {
+	// const { notes, items } = app.locals;
+	// return res.status(200).json({ notes, items });
+	res.status(200).send('Lists delivered');
+});
+
+app.post('/api/v1/lists', (req, res) => {
+	res.status(200).send('List received');
+});
+
+app.get('/api/v1/lists/:id', (req, res) => {
+	res.status(200).send('Single list delivered');
+});
+
+// app.put('/api/v1/lists/:id', (req, res) => {
+// 	res.status(200).send('Single list updated');
+// });
+
+app.patch('/api/v1/lists/:id', (req, res) => {
+	res.status(200).send('Single list updated');
+});
+
+app.delete('/api/v1/lists/:id', (req, res) => {
+	res.status(200).send('Single list deleted');
+});
 
 app.get('/api/v1/notes', (req, res) => {
-	const { notes, items } = app.locals;
-	return res.status(200).json({ notes, items });
+	res.status(200).send('Note delivered');
+});
+
+app.post('/api/v1/notes', (req, res) => {
+	res.status(200).send('Note received');
+});
+
+app.get('/api/v1/notes/:id', (req, res) => {
+	res.status(200).send('Single note delivered');
+});
+
+// app.put('/api/v1/notes/:id', (req, res) => {
+// 	res.status(200).send('Single note updated');
+// });
+
+app.patch('/api/v1/notes/:id', (req, res) => {
+	res.status(200).send('Single note updated');
+});
+
+app.delete('/api/v1/notes/:id', (req, res) => {
+	res.status(200).send('Single note deleted');
 });
 
 app.listen(3001, () => console.log('Listening on port 3001'));
