5faa043374ab47d195b8914e71994ab6170a9ce6
Niraj Aryal <niroz.aryal11@gmail.com>
2019-05-23 11:28:56 -0600

    Passes testing for get route with an id

--- app.spec.js
+++ app.spec.js
@@ -3,8 +3,9 @@ import '@babel/polyfill';
 import app from './app';
 
 describe ('api', () => {
+
   describe( 'GET /api/v1/lists', () => {
-    let lists;
+  	let lists;
     beforeEach(() => {
       lists = [
     {
@@ -46,6 +47,7 @@ describe ('api', () => {
     }
 
 ]
+    
 	app.locals.lists = lists;
     })
     it ('should have a response status of 200', async () => {
@@ -61,6 +63,40 @@ describe ('api', () => {
     })
 
   } )
+
+  describe ('GET /api/v1/lists/:id', () => {
+  	it('should have a response status of 200', async () => {
+  		const response = await request(app).get('/api/v1/lists/1558629436428');
+  		expect(response.status).toBe(200);
+  	})
+
+  	it ('should have a response status of 404 if list not found', async () => {
+  		const response = await request(app).get('/api/v1/lists/1558629436429')
+  		expect(response.status).toBe(404)
+  	})
+
+  	it ('should return matching note with the id being passed in', async () => {
+  		const response = await request(app).get('/api/v1/lists/1558629436428');
+  		const expected = {
+        "id": 1558629436428,
+        "title": "books to read",
+        "notes": [
+            {
+                "id": 1558629416536,
+                "userTask": "refactoring code",
+                "complete": false
+            },
+            {
+                "id": 1558629434028,
+                "userTask": "long walk to freedom",
+                "complete": false
+            }
+        ]
+    };
+  		console.log("expected",expected)
+  		expect (response.body).toEqual(expected);
+  	})
+  })
 
 
 
